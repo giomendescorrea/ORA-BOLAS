7a7dee3a23a085178f7ac6bedede728b7867ec66
Giovanna Mendes Corrêa <147007498+giomendescorrea@users.noreply.github.com>
2024-05-13 13:43:15 +0000

    grafico velocidade pdf

--- main.py
+++ main.py
@@ -5,52 +5,38 @@ from matplotlib.animation import FuncAnimation
 def carrega_dados_bola(arquivo):
     dados_bola = []
 
-    base_dados = open(arquivo, 'r')
-
-    for linha in base_dados:
-        dados_bola_inst_t = linha.replace("\n", "").split(";")
-
-        instante_t, x_bola, y_bola, vx_bola, vy_bola, ax_bola, ay_bola = dados_bola_inst_t
-
-        bola = {
-            "t": float(instante_t),
-            "x_bola": float(x_bola),
-            "y_bola": float(y_bola),
-            "vx_bola": float(vx_bola),
-            "vy_bola": float(vy_bola),
-            "ax_bola": float(ax_bola),
-            "ay_bola": float(ay_bola)
-        }
-
-        dados_bola.append(bola)
-
-    base_dados.close()
+    with open(arquivo, 'r') as base_dados:
+        for linha in base_dados:
+            instant_t, x_bola, y_bola, vx_bola, vy_bola, ax_bola, ay_bola = linha.strip().split(";")
+            bola = {
+                "t": float(instant_t),
+                "x_bola": float(x_bola),
+                "y_bola": float(y_bola),
+                "vx_bola": float(vx_bola),
+                "vy_bola": float(vy_bola),
+                "ax_bola": float(ax_bola),
+                "ay_bola": float(ay_bola)
+            }
+            dados_bola.append(bola)
 
     return dados_bola
 
 def carrega_dados_robo(arquivo):
     dados_robo = []
 
-    base_dados = open(arquivo, 'r')
-
-    for linha in base_dados:
-        dados_robo_inst_t = linha.replace("\n", "").split(";")
-
-        instante_t, x_robo, y_robo, vx_robo, vy_robo, ax_robo, ay_robo = dados_robo_inst_t
-
-        bola = {
-            "t": float(instante_t),
-            "x_bola": float(x_robo),
-            "y_bola": float(y_robo),
-            "vx_bola": float(vx_robo),
-            "vy_bola": float(vy_robo),
-            "ax_bola": float(ax_robo),
-            "ay_bola": float(ay_robo)
-        }
-
-        dados_robo.append(bola)
-
-    base_dados.close()
+    with open(arquivo, 'r') as base_dados:
+        for linha in base_dados:
+            instant_t, x_robo, y_robo, vx_robo, vy_robo, ax_robo, ay_robo = linha.strip().split(";")
+            robo = {
+                "t": float(instant_t),
+                "x_robo": float(x_robo),
+                "y_robo": float(y_robo),
+                "vx_robo": float(vx_robo),
+                "vy_robo": float(vy_robo),
+                "ax_robo": float(ax_robo),
+                "ay_robo": float(ay_robo)
+            }
+            dados_robo.append(robo)
 
     return dados_robo
 
@@ -60,20 +46,20 @@ def ponto_menor_distancia(dados_bola, x_robo, y_robo):
     indice_menor_dis = -1
 
     for registro in dados_bola:
-        distancia = math.sqrt(pow(registro['x_bola'] - x_robo, 2) + pow(registro['y_bola'] - y_robo, 2))
-    
+        distancia = math.sqrt((registro['x_bola'] - x_robo)**2 + (registro['y_bola'] - y_robo)**2)
+
         if distancia < menor_distancia:
             menor_distancia = distancia
             indice_menor_dis = dados_bola.index(registro)
 
-    
+
     dados_bola_menor_dist = dados_bola[indice_menor_dis]
 
     return dados_bola_menor_dist
 
 def calcula_velocidade_interceptacao(posicao_bola, posicao_robo, instante):
     velocidade = (posicao_bola - posicao_robo) / instante
-    
+
     return velocidade
 
 def calcula_aceleracao_interceptacao(velocidade, instante):
@@ -116,12 +102,10 @@ def trajetoria_robo(ponto_intercep, x_inicial, y_inicial, trajetoria_bola):
     return dados_trajetoria_robo
 
 def salva_dados_robo(dados_trajetoria):
-    arquivo = open('dados_trajetoria_robo.txt', 'w')
+    with open('dados_trajetoria_robo.txt', 'w') as arquivo:
+        for linha in dados_trajetoria:
+            arquivo.write(f"{linha['t']};{linha['x_robo']};{linha['y_robo']};{linha['vx_robo']};{linha['vy_robo']};{linha['ax_robo']};{linha['ay_robo']}\n")
 
-    for linha in dados_trajetoria:
-        arquivo.write(f"{linha['t']};{linha['x_robo']};{linha['y_robo']};{linha['vx_robo']};{linha['vy_robo']};{linha['ax_robo']};{linha['ay_robo']}\n")
-    
-    arquivo.close()
 
 def exibe_simulacao(dados_bola, dados_robo):
     x_plot_b = []
@@ -129,7 +113,7 @@ def exibe_simulacao(dados_bola, dados_robo):
     x_plot_r = []
     y_plot_r = []
 
-    for linha in  range(len(dados_robo)):
+    for linha in range(len(dados_robo)):
         xb = dados_bola[linha]['x_bola']
         yb = dados_bola[linha]['y_bola']
         xr = dados_robo[linha]['x_robo']
@@ -139,7 +123,7 @@ def exibe_simulacao(dados_bola, dados_robo):
         y_plot_b.append(yb)
         x_plot_r.append(xr)
         y_plot_r.append(yr)
-    
+
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
 
@@ -148,17 +132,20 @@ def exibe_simulacao(dados_bola, dados_robo):
     ax1.set_ylim(0, 8)
     ax2.set_ylim(0, 8)
 
+    img = plt.imread("campo.jpg")
+    ax1.imshow(img, zorder=0, extent=[0, 10, 0, 8])
+
     def update_data(frame):
-        ax1.plot(x_plot_b[:frame], y_plot_b[:frame], color='g', ms=2, ds='steps', marker='.')
-        ax2.plot(x_plot_r[:frame], y_plot_r[:frame], color='blue', ms=2, ds='steps', marker='.')
-        
-        ax1.legend(['trajetoria bola','trajetoria robo'], labelcolor=['green', 'blue'], loc= 1)
-        
-        fig.suptitle('Simulacao interceptacao', fontsize=16)
+        ax1.plot(x_plot_b[:frame], y_plot_b[:frame], color='white', ms=2, ds='steps', marker='.')
+        ax2.plot(x_plot_r[:frame], y_plot_r[:frame], color='black', ms=2, ds='steps', marker='.')
+
+        ax1.legend(['Trajetória bola','Trajetória robô'], labelcolor=['white', 'black'], loc=1)
+
+        fig.suptitle('Simulação interceptação', fontsize=16)
 
         return ax1, ax2
 
-    animation = FuncAnimation(fig = fig, func=update_data, frames=len(x_plot_r), repeat=False, interval=1)
+    animation = FuncAnimation(fig=fig, func=update_data, frames=len(x_plot_r), repeat=False, interval=1)
 
     plt.show()
 
@@ -166,45 +153,77 @@ def plota_graficos_posicao(dados_bola, dados_robo):
     tempo = []
     x_robo = []
     y_robo = []
-    
+
     x_bola = []
     y_bola = []
-    
-    for linha in  range(len(dados_robo)):
+
+    for linha in range(len(dados_robo)):
         tempo.append(dados_bola[linha]['t'])
         x_bola.append(dados_bola[linha]['x_bola'])
         y_bola.append(dados_bola[linha]['y_bola'])
-        
+
         x_robo.append(dados_robo[linha]['x_robo'])
         y_robo.append(dados_robo[linha]['y_robo'])
-        
-    fig, eixo = plt.subplots(1,2, sharey = True)
+
+    fig, eixo = plt.subplots(1, 2, sharey=True)
 
     eixo[0].set_xlabel("tempo (s)")
     eixo[1].set_xlabel("tempo (s)")
     eixo[0].set_ylabel("posicao X (m)")
     eixo[1].set_ylabel("posicao Y (m)")
 
-    
     plot_x_bola, = eixo[0].plot(tempo, x_bola)
     plot_x_robo, = eixo[0].plot(tempo, x_robo)
     plot_y_bola, = eixo[1].plot(tempo, y_bola)
     plot_y_robo, = eixo[1].plot(tempo, y_robo)
 
-    fig.legend((plot_x_bola, plot_x_robo), ('bola', 'robô'), loc='upper right')
-    
+    fig.legend((plot_x_bola, plot_x_robo), ('bola', 'robo'), loc='upper right')
+
     fig.suptitle('Posição em função do tempo', fontsize=16)
 
     plt.savefig("graficos/grafico_posicao.pdf")
-    
+
+def plota_graficos_velocidade(dados_bola, dados_robo):
+    tempo = []
+    vx_robo = []
+    vy_robo = []
+
+    vx_bola = []
+    vy_bola = []
+
+    for linha in range(len(dados_robo)):
+        tempo.append(dados_bola[linha]['t'])
+        vx_bola.append(dados_bola[linha]['vx_bola'])
+        vy_bola.append(dados_bola[linha]['vy_bola'])
+
+        vx_robo.append(dados_robo[linha]['vx_robo'])
+        vy_robo.append(dados_robo[linha]['vy_robo'])
+
+    fig, eixo = plt.subplots(1, 2, sharey=True)
+
+    eixo[0].set_xlabel("tempo (s)")
+    eixo[1].set_xlabel("tempo (s)")
+    eixo[0].set_ylabel("velocidade X (m/s)")
+    eixo[1].set_ylabel("velocidade Y (m/s)")
+
+    plot_vx_bola, = eixo[0].plot(tempo, vx_bola)
+    plot_vx_robo, = eixo[0].plot(tempo, vx_robo)
+    plot_vy_bola, = eixo[1].plot(tempo, vy_bola)
+    plot_vy_robo, = eixo[1].plot(tempo, vy_robo)
+
+    fig.legend((plot_vx_bola, plot_vx_robo), ('bola', 'robo'), loc='upper right')
+
+    fig.suptitle('Velocidade em função do tempo', fontsize=16)
+
+    plt.savefig("graficos/grafico_velocidade.pdf")
 
 def main():
     path_arq = 'dados_trajetoria_bola.txt'
     dados_bola = carrega_dados_bola(path_arq)
-    
+
     while True:
         x_robo = float(input("\nDigite a posição X do robô: "))
-        y_robo = float(input("Digite a posição Y do robo: "))
+        y_robo = float(input("Digite a posição Y do robô: "))
 
         ponto_interceptacao = ponto_menor_distancia(dados_bola, x_robo, y_robo)
 
@@ -215,6 +234,7 @@ def main():
         exibe_simulacao(dados_bola, dados_interceptacao)
 
         plota_graficos_posicao(dados_bola, dados_interceptacao)
+        plota_graficos_velocidade(dados_bola, dados_interceptacao)
 
         input_usuario = input("\nGostaria de simular novamente? (S/N) ")
 
